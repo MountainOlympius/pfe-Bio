524849eda3ab254928f292cb3286a7bc8f0d66eb
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-24 18:12:09 +0100

    Add session middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const expressSession = require('express-session')
+const PgConnectStore = require('connect-pg-simple')(expressSession)
+
+require('dotenv').config()
 
 const ApiRouter = require('./routes')
 
+// TODO : Add CORS support in development envirement
 const App = (pool) => {
     const app = express()
 
     app.use(express.json())
+
+    app.use(
+        expressSession({
+            store: new PgConnectStore({ pool }),
+            resave: false,
+            saveUninitialized: false,
+            cookie: { maxAge: 30 * 24 * 3600 },
+            secret: process.env.session_secrect || '123456'
+        })
+    )
 
     app.use('/api', ApiRouter(pool))
 
