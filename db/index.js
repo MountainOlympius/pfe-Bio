c7076354b4dd93b61bfd68e254b91b192887f0e0
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-03-14 00:13:19 +0100

    Add max connection

--- db/index.js
+++ db/index.js
@@ -3,11 +3,15 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const config = {
-    host: process.env.PG_HOST || 'localhost',
-    port: process.env.PG_PORT || 5432,
-    user: process.env.PG_USER || 'postges',
-    password: process.env.PG_PASSWORD || '',
-    database: process.env.PG_DATABASE || 'postgres'
+	host: process.env.PG_HOST || 'localhost',
+	port: process.env.PG_PORT || 5432,
+	user: process.env.PG_USER || 'postges',
+	password: process.env.PG_PASSWORD || '',
+	database: process.env.PG_DATABASE || 'postgres',
+	max: 4,
+	min: 0,
+	acquire: 30000,
+	idle: 10000,
 }
 
 const pool = new Pool(config)
