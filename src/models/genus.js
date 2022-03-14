3a76294c110c9a64c7751a3a70b4e5eccf4ed745
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-03-14 16:38:48 +0100

    Add order by id

--- src/models/genus.js
+++ src/models/genus.js
@@ -2,11 +2,11 @@ const { buildUpdateQuery, getUpdateValues } = require("../utils/sql")
 
 const selectGenuses = (pool) => {
     return async (limit = 10, offset = 0) => {
-        const query = `SELECT g.id, g.name, JSON_AGG(json_build_object ('id', gc.id, 'content', gc.content)) as criteria
+        const query = `SELECT g.id, g.name, g.description, JSON_AGG(json_build_object ('id', gc.id, 'content', gc.content)) as criteria
         FROM genus As g
         LEFT JOIN genus_criteria AS gc ON gc.genus_id = g.id
         GROUP BY g.id, g.name
-        ORDER BY g.created_date
+        ORDER BY g.created_date, g.id
         LIMIT $1 OFFSET $2;`
 
         const response = await pool.query(query, [limit, offset])
