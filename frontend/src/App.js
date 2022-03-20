62a598e956cf629d04c5786b7dc1551308348684
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-03-20 15:49:54 +0100

    Add lazy loading of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import AdminPages from './pages/Admin.pages'
-import MainPages from './pages/Main.pages'
 import AuthenticatedOnly from './components/AuthenticatedOnly'
 import AdminOnly from './components/AdminOnly'
 import LoginPage from './pages/Login.page'
 
 import './App.css'
 
+const AdminPages = React.lazy(() => import('./pages/Admin.pages'))
+const MainPages = React.lazy(() => import('./pages/Main.pages'))
+
 function App() {
     return (
-        <Routes>
-            <Route
-                path="/admin/*"
-                element={
-                    <AuthenticatedOnly>
-                        <AdminOnly>
-                            <AdminPages />
-                        </AdminOnly>
-                    </AuthenticatedOnly>
-                }
-            />
+		<Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/*" element={<MainPages />} />
-        </Routes>
-    )
+			<Route
+				path="/admin/*"
+				element={
+					<Suspense fallback={<div>...loading</div>}>
+						<AuthenticatedOnly>
+							<AdminOnly>
+								<AdminPages />
+							</AdminOnly>
+						</AuthenticatedOnly>
+					</Suspense>
+				}
+			/>
+			<Route
+				path="/*"
+				element={
+					<Suspense fallback={<div>...loading</div>}>
+						<MainPages />
+					</Suspense>
+				}
+			/>
+		</Routes>
+	)
 }
 
 export default App
